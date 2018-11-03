88c5b3789901d2d56fdf409b73f973ea1af7f7eb
Leonardo <leonardo.godoy.campos@gmail.com>
2018-11-03 01:26:06 -0300

    inicial

---
+++ ProyectoCitt/ProyectoCitt/Controllers/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using ProyectoCitt.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCitt.Controllers
+{
+    public class Conexion
+    {
+        private static Entities _entities;
+        public static Entities entities
+        {
+            get
+            {
+                if (_entities == null)
+                {
+                    _entities = new Entities();
+                }
+                return _entities;
+            }
+        }
+    }
+}
---
+++ ProyectoCitt/ProyectoCitt/Controllers/Usuario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProyectoCitt.Data;
+
+namespace ProyectoCitt.Controllers
+{
+    public class Usuario
+    {
+
+        public int idUsuario { get; set; }
+        public string nombreUsuario { get; set; }
+        public string contraseña { get; set; }
+        public int idEstado { get; set; }
+        public int idTipoUsuario { get; set; }
+
+        public Usuario()
+        {
+            idUsuario = default(int);
+            nombreUsuario = string.Empty;
+            contraseña = string.Empty;
+            idTipoUsuario = default(int);
+            idEstado = default(int);
+        }
+
+        public bool read()
+        {
+            try
+            {
+                USUARIO usuario = Conexion.entities.USUARIO.First(f => f.NOMBREUSUARIO == this.nombreUsuario);
+                this.idUsuario = Convert.ToInt32(usuario.IDUSUARIO);
+                this.nombreUsuario = usuario.NOMBREUSUARIO;
+                this.contraseña = usuario.CONTRASEÑA;
+                this.idTipoUsuario = Convert.ToInt32(usuario.IDTIPOUSUARIO);
+                this.idEstado = Convert.ToInt32(usuario.IDESTADO);
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+        }
+    }
+}
+}
